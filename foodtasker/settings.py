e65a62731eeef9571780c0d3f04dd4bf50b4fd88
Vince P <ianparulan@gmail.com>
2017-02-08 13:37:32 -0500

    modified to allow PostgreSQL as database on deployment

--- foodtasker/settings.py
+++ foodtasker/settings.py
@@ -128,3 +128,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_REDIRECT_URL = '/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
