314a60890d3b33443aae14e734554f0c285514f6
Vince P <ianparulan@gmail.com>
2017-02-09 16:37:21 -0500

    added meal list view

--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -3,6 +3,7 @@ from foodtaskerapp.forms import UserForm, RestaurantForm, UserFormForEdit, MealF
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
+from foodtaskerapp.models import Meal
 # Create your views here.
 
 
@@ -37,7 +38,9 @@ def restaurant_account(request):
 
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_meal(request):
-    return render(request, 'restaurant/meal.html')
+    meals = Meal.objects.filter(restaurant=request.user.restaurant).order_by("-id")
+
+    return render(request, 'restaurant/meal.html', {"meals": meals})
 
 
 @login_required(login_url='/restaurant/sign-in/')
