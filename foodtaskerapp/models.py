585c5a0d93d35ba393346239f164d84065cc2549
Vince P <ianparulan@gmail.com>
2017-02-10 15:44:50 -0500

    added order and order detail models

--- foodtaskerapp/models.py
+++ foodtaskerapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
@@ -43,4 +44,40 @@ class Meal(models.Model):
     price = models.IntegerField(default=0)
 
     def __str__(self):
-        return  self.name
+        return self.name
+
+
+class Order(models.Model):
+    COOKING = 1
+    READY = 2
+    ONTHEWAY = 3
+    DELIVERED = 4
+
+    STATUS_CHOICES = (
+        (COOKING, "Cooking"),
+        (READY, "Ready"),
+        (ONTHEWAY, "On the way"),
+        (DELIVERED, "Delivered"),
+    )
+
+    customer = models.ForeignKey(Customer)
+    restaurant = models.ForeignKey(Restaurant)
+    driver = models.ForeignKey(Driver)
+    address = models.CharField(max_length=500)
+    total = models.IntegerField()
+    status = models.IntegerField(choices= STATUS_CHOICES)
+    created_at = models.DateTimeField(default=timezone.now)
+    picked_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderDetail(models.Model):
+    order = models.ForeignKey(Order, related_name='order_detail')
+    meal = models.ForeignKey(Meal)
+    quantity = models.IntegerField()
+    sub_total = models.IntegerField()
+
+    def __str__(self):
+        return str(self.id)
